fde04eddb1f9185805952d690aef0ca8ad7e636f
jparaducha <paraducha_97@hotmail.com>
2022-10-20 11:33:21 -0300

    testNG

---
+++ src/main/java/MockClass.java
@@ -0,0 +1,28 @@
+public class MockClass {
+
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    public int countOddNumbersInArray(int[] array){
+        int counter = 0;
+
+        for ( int i : array){
+            if(i%2!=0){
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+
+    public boolean isDivisible(int dividend, int divisor){
+        return dividend%divisor == 0;
+    }
+
+    public boolean isDivisible(double dividend, double divisor){
+        return dividend%divisor == 0;
+    }
+
+}
